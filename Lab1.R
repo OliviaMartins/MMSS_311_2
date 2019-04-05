f2f20dbfb631c79251eb6d40fe8bfe87f97a96d7
Olivia Martins <oliviamartins2020@u.northwestern.edu>
2019-04-05 15:03:13 -0500

    finished r

--- Lab1.R
+++ Lab1.R
@@ -4,6 +4,11 @@ install.packages("dplyr")
 install.packages("stringr")
 install.packages("lubridate")
 
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(lubridate)
+
 url <- "https://www.qogdata.pol.gu.se/data/qog_std_ts_jan19.csv"
 qog <-read.csv(url, header=TRUE)
 print(dim(qog))
